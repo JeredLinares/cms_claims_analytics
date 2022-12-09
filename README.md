c630958624c7ff862a1cfa2dbbe834d450018cd4
JD Linares <JeredL@gmail.com>
2022-12-09 12:48:55 +0000

    Finished all loading scprits.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cms_claims_synth
+
+## Loading Data
+
+
+
+Citations:
+https://www.raspberrypi.com/
+https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SynPUFs/DESample01
+
+
---
+++ scripts_setup/03_load_inpatient_data.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Load data from csv carrier claims files into cms database
+# JD Linares
+# 2022 12 07
+
+
+for file_number in {1..20}
+do
+    echo "loading file $file_number"
+    
+    mysql -e "USE cms; 
+    LOAD DATA INFILE '/media/shared/syn_data/DE1_0_2008_to_2010_Inpatient_Claims_Sample_"$file_number".csv'
+    INTO TABLE inpatient
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"\""'
+    IGNORE 1 ROWS
+    ;
+    "
+done
+
+
+
